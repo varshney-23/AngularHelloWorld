fb19d5688858d88f3590f15a0662eb2167813ea1
varshney-23 <ayushivarshney40@gmail.com>
2025-03-24 15:04:45 +0530

    image added

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'AngularAssignment1' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AngularAssignment1');
+    expect(app.message).toEqual('AngularAssignment1');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,5 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   message: string = "Hello from Ayuuu!!";
+  logo: string = 'https://i.pinimg.com/736x/c6/42/6a/c6426a033e612bdea39740a1e2c7f925.jpg';
 }
